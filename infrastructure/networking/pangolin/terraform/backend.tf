d71e03d57a59ca2f8fd7b98928de084af10e8b83
4nonX <93846287+4nonX@users.noreply.github.com>
2026-03-07 00:19:50 +0100

    Update backend.tf

--- infrastructure/networking/pangolin/terraform/backend.tf
+++ infrastructure/networking/pangolin/terraform/backend.tf
@@ -4,12 +4,16 @@ terraform {
     key    = "homelab/terraform.tfstate"
     region = "us-east-1"
 
-    endpoint                    = "http://192.168.8.158:9000"
+    endpoints = {
+      s3 = "http://192.168.8.158:9000"
+    }
+
     access_key                  = "DanDressen"
     secret_key                  = "DHimHmwnmPS23"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
     force_path_style            = true
   }
 }
